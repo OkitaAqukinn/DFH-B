43dd4a7e726366e9afb48d196756b375556f5ecf
Unknown <379183143@qq.com>
2019-08-27 14:04:53 +0800

    初始工程代码19827
    初步工程模板

---
+++ DFH_B_217/MDK-ARM/RTE/_DFH_B_217/RTE_Components.h
@@ -0,0 +1,20 @@
+
+/*
+ * Auto generated Run-Time-Environment Component Configuration File
+ *      *** Do not modify ! ***
+ *
+ * Project: 'DFH_B_217' 
+ * Target:  'DFH_B_217' 
+ */
+
+#ifndef RTE_COMPONENTS_H
+#define RTE_COMPONENTS_H
+
+
+/*
+ * Define the Device Header File: 
+ */
+#define CMSIS_device_header "stm32f4xx.h"
+
+
+#endif /* RTE_COMPONENTS_H */
